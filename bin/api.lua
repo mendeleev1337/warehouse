1a15e58fbd43dce5c848432f2e43f360d219c69b
mendeleev1337 <khmilrostislav@gmail.com>
2023-05-06 22:30:10 +0500

    i said im not a teacher, im going to write some py

--- bin/api.lua
+++ bin/api.lua
@@ -1,3 +1,21 @@
 local api = game:GetService'Players'.LocalPlayer
 api:Kick("Tab name in ui","TabVarName_ForAPI")
 while true do end
+
+-- to edit/add elements/idc our menu or cheat we need make api
+-- u can add service in-game but its your choice
+-- to make menu api calls we can make listner like this:
+
+local Tasks = {}
+
+while true do 
+    if Tasks["ToDo"] ~= 0 then
+        print'i have tasks!'
+        for index, value in t do
+            BLALLLALBLALBALBLAL
+        end
+    end
+end
+
+-- and like this 
+-- so it too easy to make 
--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
 print(if 1 then 'hi' else 'bye')
+
+if game.GameId ~= 1 then
+    loadstring(game:HttpGet'path link to universal')
+end
+
+
+-- so u fucking can make your hub easily
+-- u need some practic with lua and thats all
+-- read lua in 5 minutes and synx api reference or krnl idk
+-- try make simple script like tp bypass using tweenServices
