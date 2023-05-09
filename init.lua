619a4d200a56e7e059b592915c4a9decb36196ea
mendeleev1337 <khmilrostislav@gmail.com>
2023-05-09 22:32:43 +0500

    💀💀💀

--- init.lua
+++ init.lua
@@ -3,8 +3,6 @@ print(if 1 then 'hi' else 'bye')
 if game.GameId ~= 1 then
     loadstring(game:HttpGet'path link to universal')
 end
-
-
 -- so u fucking can make your hub easily
 -- u need some practic with lua and thats all
 -- read lua in 5 minutes and synx api reference or krnl idk
